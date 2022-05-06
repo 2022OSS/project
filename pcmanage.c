e25e20505addd640bf26a7f6f760469b9cb55d07
ossking <22000223@handong.ac.kr>
2022-05-06 12:16:13 +0900

    U

--- pcmanage.c
+++ pcmanage.c
@@ -13,3 +13,21 @@ int createUser(User *u){
     printf("==> 추가됨\n"); 
     return 1;
 }
+int updateUser(User *u){
+    printf("\n");
+    printf("사용자 이름은? ");
+    scanf("%s",u->name);
+
+    printf("시작 시간은?(예:1330->13시30분) ");
+    scanf("%d",&u->s_time);
+
+    printf("김밥 주문 수량은? ");
+    scanf("%d",&u->eat[0]);
+    printf("라면 주문 수량은? ");
+    scanf("%d",&u->eat[1]);
+    printf("아메리카노 주문 수량은? ");
+    scanf("%d",&u->eat[2]);
+
+    printf("==> 수정됨\n");
+    return 1;
+}
